8172cc35ffde5a84e5d55701a23b55f63b69aefc
Christopher Kelley <christopher.r.kelley@gmail.com>
2015-04-19 16:45:35 -0400

    Initial implementation of GameDefinition.

---
+++ NetGamesFX-Core/src/main/java/netgamesfx/engine/game/GameDefinition.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netgamesfx.engine.game;
+
+/**
+ *
+ * @author Christopher
+ */
+public class GameDefinition {
+    
+    public String getTitle() {
+        return "";
+    }
+
+    public String getDescriptionAsHtml() {
+        return "<html></html>";
+    }
+}
